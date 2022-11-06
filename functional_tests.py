e3510c1d244246d5fcd96e6ae0451f853b3f15f8
ArtemHorik <artemhorik11@gmail.com>
2022-11-06 21:39:48 +0200

    functional test update 1

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Edge()
-browser.get('http://localhost:8000')
-# we see To Do title and header
-assert "To-Do!" in browser.title
 
-# нам предлагается ввести элемент списка
+class NewVisitorTest(unittest.TestCase):
+    """NewVisitor Test"""
 
-# мы набираем "Find work"
+    def setUp(self):
+        """Set up"""
+        self.browser = webdriver.Edge()
+        self.base_url = "http://localhost:8000"
 
-# после нажатия Enter страница обновляется и теперь содержит "1: Find work"
+    def tearDown(self):
+        """Tear down"""
+        self.browser.quit()
 
-# текстовое поле по-прежнему приглашает нас добавить еще один элемент
+    def test_start_a_list_and_receive_it_later(self):
+        """Start a list and receive"""
+        self.browser.get(self.base_url)
+        # we see To Do title and header
+        self.assertIn("To Do", self.browser.title)
+        self.fail("Stop test!")
 
-browser.quit()
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
