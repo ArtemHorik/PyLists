106366f113b613bb6b0d7995c77dc3004b121bdb
ArtemHorik <artemhorik11@gmail.com>
2022-11-10 18:27:25 +0200

    New URL + view for add items to existing lists. FT succeeds!

--- app/views.py
+++ app/views.py
@@ -7,14 +7,22 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
+def view_list(request, list_id):
     """List view"""
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     """Creates new list"""
     list_ = List.objects.create()
     Item.objects.create(text=request.POST.get('item_text'), list=list_)
-    return redirect('/lists/unique-list/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    """Adds item to list"""
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST.get('item_text'), list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
