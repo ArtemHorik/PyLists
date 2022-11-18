a8a33acc4010f429c51a5262c28e568f823d4023
ArtemHorik <artemhorik11@gmail.com>
2022-11-18 21:02:16 +0200

    Module tests separated in two files

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-
 from app.models import Item, List
 
 
@@ -92,34 +91,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
-
-class ListAndItemModelTest(TestCase):
-    """List item model test"""
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(len(saved_items), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
