0289ee92f94e4de02a1305c2bb32bb04d1a390a3
ArtemHorik <artemhorik11@gmail.com>
2022-11-19 21:14:13 +0200

    Model validation made in list view

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -72,6 +72,18 @@ class ListViewTest(TestCase):
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
+    def test_validation_errors_end_up_on_list_page(self):
+        """test: validation errors end up on list page"""
+        list_ = List.objects.create()
+        response = self.client.post(
+            f"/lists/{list_.id}/",
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = "You can't have an empty list item"
+        self.assertContains(response, expected_error)
+
 
 class NewListTest(TestCase):
     """NewList test"""
