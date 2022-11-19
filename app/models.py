342f3eb74523ba0ebd01cb67249c69ffeb7bdb08
ArtemHorik <artemhorik11@gmail.com>
2022-11-19 21:33:02 +0200

    get_absolute_url used in list model for deleiting URL duplicates in views

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class List(models.Model):
     """List"""
-    pass
+
+    def get_absolute_url(self):
+        """Get absolute URL"""
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
--- app/views.py
+++ app/views.py
@@ -12,17 +12,15 @@ def home_page(request):
 def view_list(request, list_id):
     """List view"""
     list_ = List.objects.get(id=list_id)
-    item = None
     error = None
 
     if request.method == 'POST':
         try:
-            item = Item.objects.create(text=request.POST.get('item_text'), list=list_)
+            item = Item(text=request.POST.get('item_text'), list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
-            item.delete()
             error = "You can't have an empty list item'"
     return render(request, 'list.html', {'list': list_, 'error': error})
 
@@ -38,4 +36,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {'error': error})
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
